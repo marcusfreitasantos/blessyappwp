6f1ccc675c180f48a78f1fff448d8132993fc270
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-03-10 11:01:29 -0300

    [FEAT] Save bookmarks to user profile

--- api/users.php
+++ api/users.php
@@ -55,15 +55,19 @@ function generateUniqueUsername($username) {
 
 function sendUserDataAfterJWTAuth($jwt, $user){
     $userAvatarUrl = wp_get_attachment_image_url( get_user_meta($user->ID, "avatar", true), "medium" );
-	$data = array(
-		'token' => $jwt['token'],
-		'userID' => $user->ID,
-		'email' => $user->user_email,
-		'firstName' => $user->first_name,
-		'lastName' => $user->last_name,
-		'avatar' => $userAvatarUrl
-	);
-	return $data;
+	if(!in_array('church', $user->roles)){
+		$data = array(
+			'token' => $jwt['token'],
+			'userID' => $user->ID,
+			'email' => $user->user_email,
+			'firstName' => $user->first_name,
+			'lastName' => $user->last_name,
+			'avatar' => $userAvatarUrl
+		);
+		return $data;
+	}else{
+		return new WP_Error( 'forbidden', "User not allowed.", array( 'status' => 403 ) );
+	}
 }
 add_filter('jwt_auth_token_before_dispatch', 'sendUserDataAfterJWTAuth', 10, 2);
 
@@ -95,5 +99,89 @@ add_action( 'rest_api_init', function () {
   register_rest_route( 'blessyapp/v2', '/users/(?P<id>\d+)', array(
     'methods' => 'POST',
     'callback' => 'updateUserById',
+  ) );
+} );
+
+
+function getUserBookmarks($req){
+	$userId = $req['id'];
+	$userBookmarkedChurches = get_user_meta($userId, 'user_bookmarked_churches', true);
+	
+	if($userBookmarkedChurches){
+		return rest_ensure_response($userBookmarkedChurches);
+	}else{
+		return new WP_Error( 'not_found', "No bookmarks found.", array( 'status' => 404 ) );
+	}
+}
+
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'blessyapp/v2', '/users/(?P<id>\d+)/bookmark', array(
+    'methods' => 'GET',
+    'callback' => 'getUserBookmarks',
+  ) );
+} );
+
+
+function saveUserBookmarks($req){
+	$userId = $req['id'];
+	$reqBody = json_decode(file_get_contents('php://input'));
+	
+	if(get_user_by('id', $userId)){
+		$userBookmarkedChurches = get_user_meta($userId, 'user_bookmarked_churches', true);
+		if($userBookmarkedChurches){
+			foreach($userBookmarkedChurches as $churchId){
+				if(!in_array($reqBody->churchId, $userBookmarkedChurches)){
+					$userBookmarkedChurches[] =  $reqBody->churchId;
+				}
+			}		
+		}else{
+			$userBookmarkedChurches = [$reqBody->churchId];
+		}
+
+		update_user_meta($userId, 'user_bookmarked_churches', $userBookmarkedChurches);
+		rest_ensure_response($userBookmarkedChurches);
+	}else{
+		return new WP_Error( 'not_found', "User not found.", array( 'status' => 404 ) );
+	}
+}
+
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'blessyapp/v2', '/users/(?P<id>\d+)/bookmark', array(
+    'methods' => 'POST',
+    'callback' => 'saveUserBookmarks',
+  ) );
+} );
+
+
+function removeUserChurchBookmarks($req){
+	$userId = $req['id'];
+	$reqBody = json_decode(file_get_contents('php://input'));
+	
+	if(get_user_by('id', $userId)){
+		$userBookmarkedChurches = get_user_meta($userId, 'user_bookmarked_churches', true);
+		$newUserBookmarkedChurches = [];
+
+		if($userBookmarkedChurches){
+			foreach ($userBookmarkedChurches as $churchId) {
+				if($churchId !== $reqBody->churchId){
+					$newUserBookmarkedChurches[] = $churchId;
+				}
+			}
+			
+			update_user_meta($userId, 'user_bookmarked_churches', $newUserBookmarkedChurches);
+			return rest_ensure_response($newUserBookmarkedChurches);
+		}
+	}else{
+		return new WP_Error( 'not_found', "User not found.", array( 'status' => 404 ) );
+	}
+}
+
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'blessyapp/v2', '/users/(?P<id>\d+)/bookmark', array(
+    'methods' => 'PUT',
+    'callback' => 'removeUserChurchBookmarks',
   ) );
 } );
