b9be9c33c382e5401407e112829ee8144babaacb
Marcus Freitas <marcusfreitasantos@gmail.com>
2025-02-27 14:28:18 -0300

    [FEAT] Integration to migrate data to firebase

--- firebase-migration.php
+++ firebase-migration.php
@@ -1,4 +1,24 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Contract\Auth;
+use Google\Cloud\Firestore\FirestoreClient;
+
+global $credentials, $factory, $auth, $firestore;
+$credentials = __DIR__ . "/google-credentials/blessy-app-firebase.json";
+$factory = (new Factory)->withServiceAccount($credentials);
+$auth = $factory->createAuth();
+
+putenv("GOOGLE_APPLICATION_CREDENTIALS=$credentials");
+
+if (!defined('FIREBASE_PROJECT_ID') || empty(FIREBASE_PROJECT_ID)) {
+    die("Error: FIREBASE_PROJECT_ID is not set or is empty.");
+}
+
+$firestore = new FirestoreClient([
+    'projectId' => FIREBASE_PROJECT_ID,
+    'database'  => '(default)'
+]);
 
 
 function firebase_migration() {
@@ -19,24 +39,162 @@ function firebase_migration_page(){
         migrate_users_to_firestore();
     };
     
+    if(isset($_POST['migrate_news'])){
+        migrate_news_to_firestore();
+    };
     ?>
 
-    <h2>Migrate Users to Firestore</h2>
     <form method='post'>
-       <?php submit_button('Migrate Users', 'primary', 'migrate_users'); ?>
+        <?php submit_button('Migrate Users', 'primary', 'migrate_users'); ?>
+    </form>
+
+    <form method='post'>
+        <?php submit_button('Migrate News', 'primary', 'migrate_news'); ?>
     </form>
 <?php }
 
 
+function insert_user_meta_data_in_firestore($user, $userUID){
+    global $firestore;
+
+    try{
+        $usersCollection = $firestore->collection('Users');
+
+        $usersCollection->add([
+            "address" => get_user_meta($user->id, "church_address", true),
+            "state" => get_user_meta($user->id, "church_logo", true),
+            "city" => get_user_meta($user->id, "church_logo", true),
+            "email" => $user->user_email,
+            "firstName" => $user->first_name,
+            "lastName" => $user->last_name,
+            "role" => $user->roles[0],
+            "userId" => $userUID,
+        ]);
+ 
+        echo "<div class='updated'><p>Created user: $user->first_name | $user->user_email | $userUID</p></div>";
+
+
+    }catch (ApiException $e) {
+        $request = $e->getRequest();
+        $response = $e->getResponse();
+    
+        echo $request->getUri().PHP_EOL;
+        echo $request->getBody().PHP_EOL;
+    
+        if ($response) {
+            echo $response->getBody();
+        }
+    }
+
+}
+
+
 function migrate_users_to_firestore(){
+    global $auth;
     $currentUsersList = get_users();
     
-
     if(!empty($currentUsersList)){
         foreach($currentUsersList as $user){
+            $userAvatarUrl = wp_get_attachment_image_url( get_user_meta($user->ID, "avatar", true), "medium" );
             $newUserPass = wp_generate_password(12, true);
-            echo "<div class='updated'><p>Name: $user->first_name</p></div>";
-            echo "<div class='updated'><p>Email: $user->user_email</p></div>";
+
+            $userProperties = [
+                'email' => $user->user_email,
+                'emailVerified' => false,
+                'password' => $newUserPass,
+                'displayName' => $user->first_name,
+                'disabled' => false,
+            ];
+            
+            try{
+                $createdUser = $auth->createUser($userProperties);
+                insert_user_meta_data_in_firestore($user, $createdUser->uid);
+            }catch(\Kreait\Firebase\Exception\Auth\EmailExists $e){
+                $errorMessage = $e->getMessage();
+                echo "<div class='error'><p>Error: $errorMessage</p></div>";
+            }
+
         };
     }
 }
+
+
+function get_user_from_firestore($userEmail){
+    global $auth;
+
+    try{
+        $user = $auth->getUserByEmail($userEmail);
+        return $user->uid;
+
+    }catch (ApiException $e) {
+        $request = $e->getRequest();
+        $response = $e->getResponse();
+    
+        echo $request->getUri().PHP_EOL;
+        echo $request->getBody().PHP_EOL;
+    
+        if ($response) {
+            echo $response->getBody();
+        }
+    }
+}
+
+function insert_news_in_firestore($newsData){
+    global $firestore;
+
+    try{
+        $usersCollection = $firestore->collection('news');
+        $usersCollection->add($newsData);
+ 
+        echo "<br><br>";
+        print_r($usersCollection);
+        echo "<br>";
+        echo "<div class='updated'><p>News created!</p></div>";
+        echo "<br><br>";
+
+
+
+    }catch (ApiException $e) {
+        $request = $e->getRequest();
+        $response = $e->getResponse();
+    
+        echo $request->getUri().PHP_EOL;
+        echo $request->getBody().PHP_EOL;
+    
+        if ($response) {
+            echo $response->getBody();
+        }
+    }
+
+}
+  
+
+function migrate_news_to_firestore(){
+    $news = get_posts(array(
+        'post_type' => 'news',
+        'post_status' => 'publish',
+        'numberposts'      => -1,
+    )); 
+
+    if(!empty($news)){
+        foreach($news as $newsPost){
+            $authorID = get_post_field( 'post_author', $newsPost->ID );
+            $authorData = get_user_by( 'id', $authorID);
+            $firebaseUserId = get_user_from_firestore($authorData->user_email);
+
+            $newsData = [
+                'postTitle' => $newsPost->post_title,
+                'postContent' => $newsPost->post_content,
+                'postExerpt' => $newsPost->post_excerpt,
+                'postDate' => $newsPost->post_date,
+                'postStatus' => 'publish',
+                'authorID' => $firebaseUserId,
+            ];
+
+            insert_news_in_firestore($newsData);
+        };
+    }
+
+}
+
+
